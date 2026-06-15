bad18b01b2c090051485c40a03d14e7827805de0
DavMunHer <davidmunyozh@gmail.com>
2026-06-15 20:25:26 +0200

    feat: added tauri command for updating project state in sqlite

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -4,5 +4,5 @@ pub mod projects;
 pub use db::db_initialize;
 pub use projects::{
     db_create_project, db_delete_project, db_get_project_by_id, db_get_project_state,
-    db_list_projects, db_toggle_project_favorite, db_update_project,
+    db_list_projects, db_toggle_project_favorite, db_update_project, db_update_project_state,
 };
